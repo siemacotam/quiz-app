26002a5cab3556b060026a9de90d6da885ea8830
Grzegorz Celek <g.celek@gmail.com>
2024-09-03 10:27:30 +0200

    project setup and general settings

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["**/tests/unit/**/*.spec.[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/"],
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: {
+        runtimeErrors: (error) => {
+          const ignoreErrors = [
+            "ResizeObserver loop limit exceeded",
+            "ResizeObserver loop completed with undelivered notifications.",
+          ];
+          if (ignoreErrors.includes(error.message)) {
+            return false;
+          }
+          return true;
+        },
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/styles/variables.scss";
+          @import "@/styles/mixins.scss";
+        `,
+      },
+    },
+  },
 });
